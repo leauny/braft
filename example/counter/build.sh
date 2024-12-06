ab24f0363c3ad7fa13c948ea5814459cc8f5dd0f
leauny <leauny@outlook.com>
2024-12-06 14:34:49 +0800

    build braft lib before run

--- example/counter/build.sh
+++ example/counter/build.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/bash
 
-if [[ $# -lt 1 ]];then
-  echo "usage: $0 BRAFT_INCLUDE_PATH"
-  exit 1
-fi
-
-set -x
-
-BRAFT_INCLUDE_PATH=$1
-
 cd $(dirname $0) || exit 2
 
+BRAFT_BUILD_DIR=$(find ../..  -type d -path "*output/include/braft" | xargs dirname | xargs dirname | xargs dirname)
+
+if [[ $BRAFT_BUILD_DIR == "" ]]; then
+    echo "braft not found"
+    exit 1
+fi
+
+if (cat $BRAFT_BUILD_DIR/CMakeCache.txt | grep ninja) &> /dev/null ; then
+    ninja -C $BRAFT_BUILD_DIR -j"$(nproc)"
+else
+    make --directory $BRAFT_BUILD_DIR -j"$(nproc)"
+fi
+
 rm -rf ./build/
-cmake -B build -DBRAFT_INCLUDE_PATH="$BRAFT_INCLUDE_PATH"
+cmake -B build
 cmake --build build -j"$(nproc)"
 
 echo "Build done."
